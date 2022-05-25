a0d4c73e5454b739950fd37cff5d9ac1cec1ad2f
sas1711006 <105028870+sas1711006@users.noreply.github.com>
2022-05-25 13:23:20 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 music.setVolume(255)
-for (let index = 0; index < 3; index++) {
-    music.startMelody(music.builtInMelody(Melodies.Nyan), MelodyOptions.Once)
-}
+music.startMelody(music.builtInMelody(Melodies.Nyan), MelodyOptions.Once)
+music.startMelody(music.builtInMelody(Melodies.Nyan), MelodyOptions.Once)
+music.startMelody(music.builtInMelody(Melodies.Nyan), MelodyOptions.Once)
 basic.forever(function () {
 	
 })
